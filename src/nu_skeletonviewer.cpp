533c30609d366cafeb23fc185d9c20725946d2e4
John Ware <jakeware@gmail.com>
2011-03-13 19:11:38 -0500

    got the skeleton wireframe displaying over the point cloud.  There is quite a bit of lag at the moment.  Not displaying the point cloud helps, but doesn't make it quite as fast as rviz.  Still need to add data capture and display options.  Another problem is that the wireframe sits behind the pointcloud of the person.  This obscures the wireframe and defeats the purpose.  Need to find another way of displaying things.

--- src/nu_skeletonviewer.cpp
+++ src/nu_skeletonviewer.cpp
@@ -36,8 +36,9 @@ using pcl::PointXYZ;
 
 // Global data
 sensor_msgs::PointCloud2ConstPtr cloud_, cloud_old_;
-nu_skeletonmsgs::SkeletonsConstPtr skels_;
+nu_skeletonmsgs::Skeletons skels_;
 boost::mutex m;
+bool gotskel = false;
 
 
 //---------------------------------------------------------------------------
@@ -58,22 +59,34 @@ private:
 public:
 
   Update() {
-    cloudsub_ = nh_.subscribe("/camera/rgb/points", 30, &Update::cloudcb, this);
+    cloudsub_ = nh_.subscribe("/camera/depth/points", 30, &Update::cloudcb, this);
     skelsub_ = nh_.subscribe("/skeletons", 1, &Update::skelcb, this);
 
     boost::thread visualization_thread (&Update::viewercb, this);
   }
 
-  void cloudcb(const sensor_msgs::PointCloud2ConstPtr &cloud){
+  pcl::PointXYZ getjoint(const nu_skeletonmsgs::SkeletonJoint &skeljoint) {
+    pcl::PointXYZ joint_position;
+      
+    joint_position.x = skeljoint.transform.translation.x;
+    joint_position.y = skeljoint.transform.translation.y;
+    joint_position.z = skeljoint.transform.translation.z;
+
+    return joint_position;
+  }
+
+  void cloudcb(const sensor_msgs::PointCloud2ConstPtr &cloud) {
     m.lock ();
     cloud_ = cloud;
     m.unlock ();
   }
   
-  void skelcb(const nu_skeletonmsgs::SkeletonsConstPtr &skels){
+  void skelcb(const nu_skeletonmsgs::Skeletons skels) {
     m.lock ();
     skels_ = skels;
     m.unlock();
+
+    gotskel = true;
   }
 
   void viewercb() {
@@ -127,88 +140,150 @@ public:
       p.removeShape ("left elbow");
       p.removeShape ("right hand");
       p.removeShape ("left hand");
-      p.removeShape ("torso");
-      p.removeShape ("right hip");
-      p.removeShape ("left hip");
+      p.removeShape ("torso RS");
+      p.removeShape ("torso LS");
+      p.removeShape ("torso RH");
+      p.removeShape ("torso LH");
+      p.removeShape ("hips");
       p.removeShape ("right knee");
       p.removeShape ("left knee");
       p.removeShape ("right foot");
       p.removeShape ("left foot");
-    
-      PointCloud<PointXYZ> testcloud;
-      
-      testcloud.points.resize (5);
-      for (size_t i = 0; i < testcloud.points.size (); ++i) {
-	testcloud.points[i].x = i; testcloud.points[i].y = i / 2; testcloud.points[i].z = 0;
-      }
-	
+            
       // If no RGB data present, use a simpler white handler
       if (rgb && pcl::getFieldIndex (cloud_xyz_rgb, "rgb", fields) != -1) {
 	pcl_visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> color_handler (cloud_xyz_rgb);
 	p.addPointCloud (cloud_xyz_rgb, color_handler, "cloud");
 
 	// add wireframe to viewer if data is being trasmitted
-	if (&skels_.size() =! 0) {
+	if (gotskel) {
 	  // add head
-	  p.addLine<PointXYZ, PointXYZ> (skels_[0].head.transform.translation, skels_[0].neck.transform.translation, 0.0, 1.0, 0.0);
-	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "head");
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].head), getjoint(skels_.skeletons[0].neck), 0.0, 1.0, 0.0, "head");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "head");	  	  
 
 	  // add right shoulder
-	  p.addLine<PointXYZ, PointXYZ> (testcloud.points[0], testcloud.points[1], 0.0, 1.0, 0.0);
-	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "right shoulder");
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].right_shoulder), getjoint(skels_.skeletons[0].neck), 0.0, 1.0, 0.0, "right shoulder");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "right shoulder");	  
 
 	  // add left shoulder
-	  p.addLine<PointXYZ, PointXYZ> (testcloud.points[0], testcloud.points[1], 0.0, 1.0, 0.0);
-	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "left shoulder");
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].left_shoulder), getjoint(skels_.skeletons[0].neck), 0.0, 1.0, 0.0, "left shoulder");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "left shoulder");	  
 
 	  // add right elbow
-	  p.addLine<PointXYZ, PointXYZ> (testcloud.points[0], testcloud.points[1], 0.0, 1.0, 0.0);
-	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "right elbow");
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].right_elbow), getjoint(skels_.skeletons[0].right_shoulder), 0.0, 1.0, 0.0, "right elbow");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "right elbow");	  
 
 	  // add left elbow
-	  p.addLine<PointXYZ, PointXYZ> (testcloud.points[0], testcloud.points[1], 0.0, 1.0, 0.0);
-	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "left elbow");
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].left_elbow), getjoint(skels_.skeletons[0].left_shoulder), 0.0, 1.0, 0.0, "left elbow");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "left elbow");	  
 
 	  // add right hand
-	  p.addLine<PointXYZ, PointXYZ> (testcloud.points[0], testcloud.points[1], 0.0, 1.0, 0.0);
-	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "right hand");
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].right_hand), getjoint(skels_.skeletons[0].right_elbow), 0.0, 1.0, 0.0, "right hand");	
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "right hand");  
 
 	  // add left hand
-	  p.addLine<PointXYZ, PointXYZ> (testcloud.points[0], testcloud.points[1], 0.0, 1.0, 0.0);
-	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "left hand");
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].left_hand), getjoint(skels_.skeletons[0].left_elbow), 0.0, 1.0, 0.0, "left hand");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "left hand");	  
 
 	  // add torso
-	  p.addLine<PointXYZ, PointXYZ> (testcloud.points[0], testcloud.points[1], 0.0, 1.0, 0.0);
-	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "torso");
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].torso), getjoint(skels_.skeletons[0].right_shoulder), 0.0, 1.0, 0.0, "torso RS");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "torso RS");
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].torso), getjoint(skels_.skeletons[0].left_shoulder), 0.0, 1.0, 0.0, "torso LS");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "torso LS");
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].torso), getjoint(skels_.skeletons[0].right_hip), 0.0, 1.0, 0.0, "torso RH");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "torso RH");
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].torso), getjoint(skels_.skeletons[0].left_hip), 0.0, 1.0, 0.0, "torso LH");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "torso LH");	  
 
-	  // add right hip
-	  p.addLine<PointXYZ, PointXYZ> (testcloud.points[0], testcloud.points[1], 0.0, 1.0, 0.0);
-	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "right hip");
-
-	  // add left hip
-	  p.addLine<PointXYZ, PointXYZ> (testcloud.points[0], testcloud.points[1], 0.0, 1.0, 0.0);
-	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "left hip");
+	  // add hips
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].right_hip), getjoint(skels_.skeletons[0].left_hip), 0.0, 1.0, 0.0, "hips");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "hips");
 
 	  // add right knee
-	  p.addLine<PointXYZ, PointXYZ> (testcloud.points[0], testcloud.points[1], 0.0, 1.0, 0.0);
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].right_knee), getjoint(skels_.skeletons[0].right_hip), 0.0, 1.0, 0.0, "right knee");
 	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "right knee");
 
 	  // add left knee
-	  p.addLine<PointXYZ, PointXYZ> (testcloud.points[0], testcloud.points[1], 0.0, 1.0, 0.0);
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].left_knee), getjoint(skels_.skeletons[0].left_hip), 0.0, 1.0, 0.0, "left knee");
 	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "left knee");
 
 	  // add right foot
-	  p.addLine<PointXYZ, PointXYZ> (testcloud.points[0], testcloud.points[1], 0.0, 1.0, 0.0);
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].right_foot), getjoint(skels_.skeletons[0].right_knee), 0.0, 1.0, 0.0, "right foot");
 	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "right foot");
-	  
+
 	  // add left foot
-	  p.addLine<PointXYZ, PointXYZ> (testcloud.points[0], testcloud.points[1], 0.0, 1.0, 0.0);
-	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "left foot");	
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].left_foot), getjoint(skels_.skeletons[0].left_knee), 0.0, 1.0, 0.0, "left foot");	  
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "left foot");
+	  	  
+	  gotskel = false;
 	}
       }
       else {
 	pcl_visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> color_handler (cloud_xyz, 255, 0, 255);
 	p.addPointCloud (cloud_xyz, color_handler, "cloud");
+
+	// add wireframe to viewer if data is being trasmitted
+	if (gotskel) {
+	  // add head
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].head), getjoint(skels_.skeletons[0].neck), 0.0, 1.0, 0.0, "head");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "head");	  	  
+
+	  // add right shoulder
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].right_shoulder), getjoint(skels_.skeletons[0].neck), 0.0, 1.0, 0.0, "right shoulder");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "right shoulder");	  
+
+	  // add left shoulder
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].left_shoulder), getjoint(skels_.skeletons[0].neck), 0.0, 1.0, 0.0, "left shoulder");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "left shoulder");	  
+
+	  // add right elbow
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].right_elbow), getjoint(skels_.skeletons[0].right_shoulder), 0.0, 1.0, 0.0, "right elbow");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "right elbow");	  
+
+	  // add left elbow
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].left_elbow), getjoint(skels_.skeletons[0].left_shoulder), 0.0, 1.0, 0.0, "left elbow");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "left elbow");	  
+
+	  // add right hand
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].right_hand), getjoint(skels_.skeletons[0].right_elbow), 0.0, 1.0, 0.0, "right hand");	
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "right hand");  
+
+	  // add left hand
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].left_hand), getjoint(skels_.skeletons[0].left_elbow), 0.0, 1.0, 0.0, "left hand");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "left hand");	  
+
+	  // add torso
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].torso), getjoint(skels_.skeletons[0].right_shoulder), 0.0, 1.0, 0.0, "torso RS");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "torso RS");
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].torso), getjoint(skels_.skeletons[0].left_shoulder), 0.0, 1.0, 0.0, "torso LS");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "torso LS");
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].torso), getjoint(skels_.skeletons[0].right_hip), 0.0, 1.0, 0.0, "torso RH");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "torso RH");
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].torso), getjoint(skels_.skeletons[0].left_hip), 0.0, 1.0, 0.0, "torso LH");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "torso LH");	  
+
+	  // add hips
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].right_hip), getjoint(skels_.skeletons[0].left_hip), 0.0, 1.0, 0.0, "hips");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "hips");
+
+	  // add right knee
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].right_knee), getjoint(skels_.skeletons[0].right_hip), 0.0, 1.0, 0.0, "right knee");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "right knee");
+
+	  // add left knee
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].left_knee), getjoint(skels_.skeletons[0].left_hip), 0.0, 1.0, 0.0, "left knee");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "left knee");
+
+	  // add right foot
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].right_foot), getjoint(skels_.skeletons[0].right_knee), 0.0, 1.0, 0.0, "right foot");
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "right foot");
+
+	  // add left foot
+	  p.addLine<PointXYZ, PointXYZ> (getjoint(skels_.skeletons[0].left_foot), getjoint(skels_.skeletons[0].left_knee), 0.0, 1.0, 0.0, "left foot");	  
+	  p.setShapeRenderingProperties (pcl_visualization::PCL_VISUALIZER_LINE_WIDTH, 5, "left foot");
+	  	  
+	  gotskel = false;
+	}
       }
     }
   }
